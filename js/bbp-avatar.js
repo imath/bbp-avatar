4048f4c89cec0644ed8b045dbe5f7dcd1b913e95
imath <math.viet@gmail.com>
2018-10-04 11:32:08 +0200

    Add some style to the button to controle contrast
    The text was not visible in TwentySeventeen

--- js/bbp-avatar.js
+++ js/bbp-avatar.js
@@ -1,16 +1,20 @@
 jQuery(document).ready(function($){
-  
-  var file_frame;
-  var wp_media_post_id = wp.media.model.settings.post.id; // Store the old id
-  var set_to_post_id = bbpavatar_vars.post_id; // Set this
 
-  $('.stag-metabox-table .button').on('click', function( event ){
+  var file_frame, wp_media_post_id = wp.media.model.settings.post.id, set_to_post_id = bbpavatar_vars.post_id,
+      avatarBtn = $('.stag-metabox-table .button');
+
+  avatarBtn.css( {
+    color: 'rgb(34, 34, 34)',
+    'background-color': 'rgb(249, 249, 249)'
+  } );
+
+  avatarBtn.on('click', function( event ){
 
     event.preventDefault();
 
     var button = $(this);
     var id = button.attr('id').replace('_button', '');
-    
+
     // If the media frame already exists, reopen it.
     if ( file_frame ) {
       // Set the post ID to what we want
@@ -39,7 +43,7 @@ jQuery(document).ready(function($){
     file_frame.on( 'select', function() {
       // We set multiple to false so only get one image from the uploader
       attachment = file_frame.state().get('selection').first().toJSON();
-  
+
       // Do something with attachment.id and/or attachment.url here
       $( "#"+id ).val( attachment.id );
 
@@ -54,9 +58,9 @@ jQuery(document).ready(function($){
       }
 
       $( "#"+id+"_thumbnail" ).val( url );
-      
+
       $("#preview").html( '<img src="' + url + '" width="'+width+'px" height="'+height+'px">' );
-      
+
       // Restore the main post ID
       wp.media.model.settings.post.id = wp_media_post_id;
     });
@@ -64,7 +68,7 @@ jQuery(document).ready(function($){
     // Finally, open the modal
     file_frame.open();
   });
-  
+
   // Restore the main ID when the add media button is pressed
   $('a.add_media').on('click', function() {
     wp.media.model.settings.post.id = wp_media_post_id;
